477d0e65a05c18f07f9b315843cce8389d179e49
Duy Thanh <thanhdd.it@gmail.com>
2012-07-24 11:42:08 +0700

    Khoi tao

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GamesStore::Application.config.secret_token = '87f618440367ed0e9dc53c5183a7ff34a547953089b1de3fdce813de8434b89ab5f23abb1d27783a5beb3a9cd4dc628aebea6fdda0c9456d7a43abcd55d8a0dc'
